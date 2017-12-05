387c04d0411e3ac1a9878d8fe723ba87324c81da
vinothsundararajan <vinothsundararajan@outlook.com>
2017-12-05 16:25:44 +0530

    windows-jenkins

---
+++ jenkins_windows/windows.bat
@@ -0,0 +1,9 @@
+git status
+git config --global user.email "Username@email.com"
+git config --global user.name "Username"
+git remote add origin https://github.com/Username/gitrepname
+echo "First file">My_file.txt
+git add .
+git checkout master
+git commit -am "testing"
+git push https://Username:Passwordxyz@github.com/Username/gitrepname --all
